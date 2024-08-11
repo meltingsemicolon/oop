1782c2ca6941ceaeb6ad2ddd5db55ebd74e45e3d
meltingsemicolon <meltingsemicolon@gmail.com>
2024-08-11 21:48:39 +0545

    lab 1 question 8

---
+++ lab1/8.cpp
@@ -0,0 +1,10 @@
+//WAP by using the ++i and i++ concept.
+#include <iostream>
+using namespace std;
+int main (){
+    int i=5;
+    cout<<"Pre-increment: "<<++i<<endl;
+    i=5;
+    cout<<"post-increment: "<<i++<<endl;
+    
+}
