7612ca3eb67afc3b7cdb6fab309284f95be6aca5
meltingsemicolon <meltingsemicolon@gmail.com>
2024-08-11 21:48:08 +0545

    lab 1 question 7

---
+++ lab1/7.cpp
@@ -0,0 +1,10 @@
+// WAP using const keyword.
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int x = 34;
+    cout << "Constant variable " << endl;
+    cout << "The value of num: " << x << endl;
+    return 0;
+}
