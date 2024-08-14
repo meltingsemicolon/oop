6c555f22c66a9dd4497b6b49e35b8601e8451b58
meltingsemicolon <meltingsemicolon@gmail.com>
2024-08-14 17:46:40 +0545

    lab 7 question 3

---
+++ lab7/3.cpp
@@ -0,0 +1,13 @@
+//WAP for Concatenating string objects->s1+s2
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1="conentate";
+    string s2=" string";
+    s1+=s2;
+    cout<< s1<<endl;
+
+}
